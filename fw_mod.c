bc17fcd5aeda0a9ced2efa21a9c1d78716ff936d
ebber <erik.beitel@gmail.com>
2018-11-21 22:27:55 -0500

    drop firewall

--- fw_mod.c
+++ fw_mod.c
@@ -13,14 +13,46 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Erik Beitel");
 MODULE_DESCRIPTION("A skeleton Linux module for a firewall.");
 MODULE_VERSION("0.01");
-static int __init mod_init(void) {
- 	printk(KERN_INFO "Hello, World!\n");
- 	return 0;
+
+
+static struct nf_hook_ops nfho; 
+
+//function to be called by hook
+unsigned int hook_func_incoming(void *priv, 
+                                struct sk_buff *skb, 
+                                const struct nf_hook_state *state)
+{
+  return NF_DROP;           /* Drop ALL packets */
 }
 
-static void __exit mod_exit(void) {
-	printk(KERN_INFO "Goodbye, World!\n");
+//Called when module loaded using 'insmod'
+int init_module()
+{
+  //function to call when conditions below met
+  nfho.hook = hook_func_incoming; 
+
+  //called right after packet received, first hook in Netfilter
+  nfho.hooknum = NF_INET_PRE_ROUTING; 
+  
+  //IPV4 packets
+  nfho.pf = PF_INET;                          
+  
+  //set to highest priority over all other hook functions
+  nfho.priority = NF_IP_PRI_FIRST;  
+  
+  //register hook
+  nf_register_net_hook(&nfho);                    
+  
+  printk(KERN_INFO "simple firewall loaded\n");
+  return 0;
 }
 
-module_init(mod_init);
-module_exit(mod_exit);
+//Called when module unloaded using 'rmmod'
+void cleanup_module()
+{ 
+  printk("simple firewall unloaded\n");
+  nf_unregister_net_hook(&nfho);                //cleanup and unregister hook
+}
+
+//module_init(mod_init);
+//module_exit(mod_exit);
